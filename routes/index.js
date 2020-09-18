61ff85c49246c2489c1b7c1d6def9a2686687ea7
saurabh <cosmiupadhyay1105@gmail.com>
2020-09-19 01:39:22 +0530

    adding a file

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('Home/index');
+router.get("/", function (req, res, next) {
+  res.render("Home/index");
 });
 
-
-router.get('/opportunities', function(req, res, next) {
-  res.render('Home/opportunities');
+router.get("/opportunities", function (req, res, next) {
+  res.render("Home/opportunities");
 });
 
-
-router.get('/blogs', function(req, res, next) {
-  res.render('Home/opportunities');
+router.get("/blogs", function (req, res, next) {
+  res.render("Home/blogs");
 });
-
 
 module.exports = router;
